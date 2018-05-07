dfbf9fe83336113b7e4e9d384c888eb115a917e3
Alana Farkas <alana.farkas@zocdoc.com>
2018-05-07 11:44:15 -0400

    updating state!

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+    constructor(props) {
+    super(props);    
+    // define the internal state of the component
+    this.state = {
+      count: this.props.start || 0
+    }
+
+    this.up = this.up.bind(this);
+    // this.down = this.down.bind(this);
+    // this.alana = this.alana.bind(this);
+  }
+
+  up() {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.up}>Hit Me</button>
+      </div>
+    );
+  }
+}
+
+export default App;
