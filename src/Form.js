e3d5b56aa6b411ba88cb6515a7b4de33f4f320a7
Alana Farkas <alana.farkas@zocdoc.com>
2018-05-10 15:56:19 -0400

    renders form to the page

---
+++ src/Form.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+class InfoForm extends Component {
+
+	render () {
+		const { myInfo } = this.props
+
+		return (
+			<form>
+				<input type='text' name="name" placeholder="Enter your name" />
+				<input type='text' name="age" placeholder="Enter your age" /> 
+				<input type='text' name="residence" placeholder="Enter your borough" /> 			 
+			</form>
+		);
+	}
+}
+
+
+export { InfoForm as default, InfoForm };
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import InfoForm from './Form';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<InfoForm />, document.getElementById('root'));
 registerServiceWorker();
